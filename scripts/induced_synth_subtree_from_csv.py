204a8e569d757a9f8858b64d304f781b2ebaa082
snacktavish <ejmctavish@gmail.com>
2022-10-31 16:52:42 -0700

    return error

--- scripts/induced_synth_subtree_from_csv.py
+++ scripts/induced_synth_subtree_from_csv.py
@@ -10,6 +10,7 @@ Will generate a tree, a logfile and a citations file, all in the output director
 
 import argparse
 import os
+import sys
 import json
 import requests
 import dendropy
@@ -18,7 +19,8 @@ from opentree import OT, annotations
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-q","---query", help="File containing ott_ids. First column should have the header 'ott_id' and contain OpenTree taxon ids")
+parser.add_argument("-q","--query", help="File containing ott_ids. First column should have the header 'ott_id' and contain OpenTree taxon ids")
+parser.add_argument("-m","--max-age", help="estimate for root age")
 parser.add_argument("-o","--output_dir", default="synth_output", help="output folder. default is synth_dir")
 parser.add_argument("-l","--label_format", default="name_and_id",help="label format. One of 'name', 'id', 'name_and_id'. Default is 'name_and_id'")
 
@@ -30,19 +32,14 @@ assert os.path.exists(args.query)
 if not os.path.exists(args.output_dir):
     os.mkdir(args.output_dir)
 
-queryfile = open(args.query)
-header = queryfile.readline()
-assert header.split(',')[0] == 'ott_id'
-
-
-
-taxa = {}
 
 assert os.path.exists(args.query)
 
 queryfile = open(args.query)
 header = queryfile.readline().strip().split(',')
+assert header[0] == 'ott_id'
 
+taxa = {}
 for lin in queryfile:
     lii = lin.strip().split(',')
     taxa['ott'+lii[0]]=dict(zip(header, lii))
@@ -53,6 +50,7 @@ queryfile.close()
 node_ids = list(taxa.keys())
 ott_ids = [node.strip('ott') for node in node_ids]
 
+## Get labelled synth tree
 ret = labelled_induced_synth(ott_ids = ott_ids,
                              label_format = args.label_format,
                              inc_unlabelled_mrca=False,
@@ -70,17 +68,25 @@ log.close()
 
 cites_file = open("{}/citations.txt".format(args.output_dir), 'w')
 citations = OT.get_citations(ret['supporting_studies'])
-
 cites_file.write(citations)
 
 
+## Get Dated synth tree
 session = requests.Session()
-
 url     = 'https://dates.opentreeoflife.org/v4/dates/dated_tree'
+#url =  'http://127.0.0.1:1983/v4/dates/dated_tree'
+
 payload = { "node_ids" : node_ids }
+if args.max_age:
+    payload["max_age"] = args.max_age
 
 resp = session.post(url=url, data=json.dumps(payload))
 resp_dict = json.loads(resp.content.decode())
+
+if resp.status_code != 200:
+    sys.stderr.write(resp.content.decode())
+    sys.exit()
+
 
 dated_tree = dendropy.Tree.get(data=resp_dict['dated_trees_newick_list'][0], schema="newick")
 dated_tree.write(path=args.output_dir+"/ottid_dated_tree.tre", schema='newick')
@@ -92,20 +98,15 @@ date_citations = OT.get_citations(ret['supporting_studies'])
 
 date_cites_file.write(date_citations)
 
-
-
-
 node_annotations = annotations.generate_synth_node_annotation(dated_tree)
 
-translation_dict = {}
-for node in node_annotations:
-    if node in taxa:
-        translation_dict[node]=taxa[node]['unique_name']
-
-annotations.write_itol_relabel(translation_dict, filename=args.output_dir+"/ottlabel.txt")
-
-for tip in dated_tree.leaf_node_iter():
-    tip.taxon.label = translation_dict[tip.taxon.label]
+#translation_dict = {}
+#for node in node_annotations:
+#    if node in taxa:
+#        translation_dict[node]=taxa[node]['unique_name']
+#annotations.write_itol_relabel(translation_dict, filename=args.output_dir+"/ottlabel.txt")
+#for tip in dated_tree.leaf_node_iter():
+#    tip.taxon.label = translation_dict[tip.taxon.label]
 
 
 dated_tree.write(path=args.output_dir+"/ott_label_dated_tree.tre", schema='newick')
